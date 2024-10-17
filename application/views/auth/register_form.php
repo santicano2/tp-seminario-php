79347ef79218129bd735635db12528ac56acf590
Santi <canosantiago404@gmail.com>
2024-10-17 17:30:55 -0300

    creados archivos necesarios para implementar el auth

---
+++ application/views/auth/register_form.php
@@ -0,0 +1,19 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<h1 class="text-white my-5"><?php echo $title; ?></h1>
+<form action="" method="POST" class="text-light bg-dark rounded-4 border border-light p-4">
+  <div class="mb-3">
+    <label for="email" class="form-label">Email:</label>
+    <input type="email" class="form-control bg-white text-dark border" id="email" name="email" placeholder="Ingrese su email">
+  </div>
+  <div class="mb-3">
+    <label for="password" class="form-label">Contraseña:</label>
+    <input type="password" class="form-control bg-white text-dark border" id="password" name="password" placeholder="Ingrese una contraseña">
+  </div>
+  <div class="mb-3">
+    <label for="confirm-password" class="form-label">Confirmar Contraseña:</label>
+    <input type="password" class="form-control bg-white text-dark border" id="confirm-password" name="confirm-password" placeholder="Confirmar contraseña">
+  </div>
+  <div class="d-flex justify-content-center p-2">
+    <button type="submit" class="btn btn-primary">Registrarse</button>
+  </div>
+</form>
