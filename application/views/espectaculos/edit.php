3332744d4ab0bce5df3089de615c0469c3b1ae08
Santi <canosantiago404@gmail.com>
2024-10-20 10:15:17 -0300

    validación para editar espectaculos

--- application/views/espectaculos/edit.php
+++ application/views/espectaculos/edit.php
@@ -3,15 +3,15 @@
 <form action="<?php echo base_url('espectaculos/update/') . $espectaculo->id; ?>" method="POST" class="text-light bg-dark rounded-0 border border-2 border-primary p-4">
   <div class="mb-3">
     <label for="name" class="form-label">Nombre:</label>
-    <input type="text" class="form-control bg-white text-dark border" id="name" name="name" value="<?php echo $espectaculo->name; ?>" placeholder="Ingrese el nombre">
+    <input type="text" class="form-control bg-white text-dark border" id="name" name="name" value="<?php echo $espectaculo->name; ?>" placeholder="Ingrese el nombre" required>
   </div>
   <div class="mb-3">
     <label for="tickets" class="form-label">Tickets:</label>
-    <input type="number" class="form-control bg-white text-dark border" id="tickets" name="tickets" value="<?php echo $espectaculo->tickets; ?>" placeholder="Ingrese el numero de tickets">
+    <input type="number" class="form-control bg-white text-dark border" id="tickets" name="tickets" value="<?php echo $espectaculo->tickets; ?>" placeholder="Ingrese el numero de tickets" required>
   </div>
   <div class="mb-3">
     <label for="price" class="form-label">Precio:</label>
-    <input type="number" class="form-control bg-white text-dark border" id="price" name="price" value="<?php echo $espectaculo->price; ?>" placeholder="Ingrese el precio">
+    <input type="number" class="form-control bg-white text-dark border" id="price" name="price" value="<?php echo $espectaculo->price; ?>" placeholder="Ingrese el precio" required>
   </div>
   <div class="d-flex justify-content-center p-2">
     <button type="submit" class="btn btn-primary">Actualizar espectáculo</button>
